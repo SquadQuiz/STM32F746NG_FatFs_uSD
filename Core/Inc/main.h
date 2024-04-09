3e8f66d28c28d701f7b5f7a566ef33ba2a15a364
SquadQuiz <amornsakstp6@gmail.com>
2024-04-09 15:35:51 +0700

    Initial Commit
    STM32F746G Discovery Board + FatFs + uSD Example

---
+++ Core/Inc/main.h
@@ -0,0 +1,87 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SDMMC_CK_Pin GPIO_PIN_12
+#define SDMMC_CK_GPIO_Port GPIOC
+#define SDMMC_D3_Pin GPIO_PIN_11
+#define SDMMC_D3_GPIO_Port GPIOC
+#define SDMMC_D2_Pin GPIO_PIN_10
+#define SDMMC_D2_GPIO_Port GPIOC
+#define uSD_Detect_Pin GPIO_PIN_13
+#define uSD_Detect_GPIO_Port GPIOC
+#define SDMMC_CMD_Pin GPIO_PIN_2
+#define SDMMC_CMD_GPIO_Port GPIOD
+#define LD1_Pin GPIO_PIN_1
+#define LD1_GPIO_Port GPIOI
+#define RCC_OSC32_IN_Pin GPIO_PIN_15
+#define RCC_OSC32_IN_GPIO_Port GPIOC
+#define RCC_OSC32_OUT_Pin GPIO_PIN_0
+#define RCC_OSC32_OUT_GPIO_Port GPIOH
+#define OSC_25M_Pin GPIO_PIN_1
+#define OSC_25M_GPIO_Port GPIOH
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ README.md
@@ -0,0 +1,1 @@
+# STM32F746G Discovery Board + FatFs + uSD (CubeMX Example)
